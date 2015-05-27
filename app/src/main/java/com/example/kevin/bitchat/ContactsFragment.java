18ce0e6c878f8151f9889d4bbf3494d0777f46fe
kcrimi <kevincrimi@gmail.com>
2015-05-27 17:44:20 -0400

    use simple cursor adapter to populate list of contacts

--- app/src/main/java/com/example/kevin/bitchat/ContactsFragment.java
+++ app/src/main/java/com/example/kevin/bitchat/ContactsFragment.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 /**
@@ -28,20 +30,23 @@ public class ContactsFragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_contacts, null);
+        ListView listView = (ListView)v.findViewById(R.id.list);
 
         String[] columns = {ContactsContract.CommonDataKinds.Phone.NUMBER,
                 ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
-
+        int[] ids = {R.id.number,R.id.name};
         Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                columns,null,null,null);
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            String number = cursor.getString(0);
-            String name = cursor.getString(1);
-            Log.d(TAG, name + " " + number);
-            cursor.moveToNext();
-        }
-        cursor.close();
+                new String[]{ContactsContract.CommonDataKinds.Phone._ID,ContactsContract.CommonDataKinds.Phone.NUMBER,
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},null,null,null);
+
+        listView.setAdapter(new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.contact_list_item,
+                cursor,
+                columns,
+                ids,
+                0
+        ));
 
         return v;
     }
