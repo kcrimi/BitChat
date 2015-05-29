42132dbb75fbafebb5ed90c60e2f8faf94a22609
kcrimi <kevincrimi@gmail.com>
2015-05-29 14:23:40 -0700

    Pull messages from both the recipient and sender or a chat

--- app/src/main/java/com/example/kevin/bitchat/MessageDataSource.java
+++ app/src/main/java/com/example/kevin/bitchat/MessageDataSource.java
@@ -23,15 +23,27 @@ public class MessageDataSource {
     }
 
     public static void fetchMessages(String sender, String recipient, final Listener listener){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Message");
-        query.whereEqualTo("sender", sender);
-        query.whereEqualTo("recipient", recipient);
-        query.findInBackground(new FindCallback<ParseObject>() {
+        ParseQuery<ParseObject> querySent = ParseQuery.getQuery("Message");
+        querySent.whereEqualTo("sender", sender);
+        querySent.whereEqualTo("recipient", recipient);
+
+        ParseQuery<ParseObject> queryReceived = ParseQuery.getQuery("Message");
+        queryReceived.whereEqualTo("recipient", sender);
+        queryReceived.whereEqualTo("sender", recipient);
+
+        List<ParseQuery<ParseObject>> queries = new ArrayList<>();
+        queries.add(querySent);
+        queries.add(queryReceived);
+
+        ParseQuery<ParseObject> mainQuery = ParseQuery.or(queries);
+        mainQuery.orderByAscending("createdAt");
+
+        mainQuery.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 ArrayList<Message> messages = new ArrayList<Message>();
-                for (ParseObject parseObject: list){
-                    Message message = new Message((String)parseObject.get("text"),(String)parseObject.get("sender"));
+                for (ParseObject parseObject : list) {
+                    Message message = new Message((String) parseObject.get("text"), (String) parseObject.get("sender"));
                     messages.add(message);
                 }
                 listener.onFetchedMessages(messages);
